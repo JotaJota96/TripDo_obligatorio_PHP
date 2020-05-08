7d49b73076cf130fb03237a4ef3ddc85d271e7f5
JotaJota96 <jjap96@gmail.com>
2020-05-08 01:16:17 -0300

    agregados scripts de base de datos

---
+++ documentacion/base_de_datos_DDL.sql
@@ -0,0 +1,313 @@
+--
+-- WARNING: The 'tripdo' database must be created before running this script
+--
+
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-05-2020 a las 03:58:57
+-- Versión del servidor: 10.4.8-MariaDB
+-- Versión de PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tripdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `colaborador`
+--
+
+CREATE TABLE `colaborador` (
+  `idUsuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `idViaje` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `destino`
+--
+
+CREATE TABLE `destino` (
+  `id` int(11) NOT NULL,
+  `pais` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `ciudad` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `idViaje` int(11) NOT NULL,
+  `agregadoPor` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `fechaAgregado` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `destinovotado`
+--
+
+CREATE TABLE `destinovotado` (
+  `idUsuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `idViaje` int(11) NOT NULL,
+  `idDestino` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plan`
+--
+
+CREATE TABLE `plan` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish2_ci NOT NULL,
+  `latitud` float(10,6) NOT NULL,
+  `longitud` float(10,6) NOT NULL,
+  `link` varchar(200) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `idDestino` int(11) NOT NULL,
+  `agregadoPor` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `fechaAgregado` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `planvotado`
+--
+
+CREATE TABLE `planvotado` (
+  `idUsuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `idViaje` int(11) NOT NULL,
+  `idPlan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tag`
+--
+
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL,
+  `texto` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `idDestino` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `nickname` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
+  `contrasenia` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `biografia` text COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `imagen` varchar(255) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `verificado` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje`
+--
+
+CREATE TABLE `viaje` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish2_ci NOT NULL,
+  `publico` tinyint(1) NOT NULL,
+  `realizado` tinyint(1) NOT NULL DEFAULT 0,
+  `idUsuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viajero`
+--
+
+CREATE TABLE `viajero` (
+  `idUsuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `idViaje` int(11) NOT NULL,
+  `valoracion` int(11) DEFAULT NULL,
+  `texto` varchar(300) COLLATE utf8_spanish2_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `colaborador`
+--
+ALTER TABLE `colaborador`
+  ADD PRIMARY KEY (`idUsuario`,`idViaje`),
+  ADD KEY `fk_colaborador_idviaje` (`idViaje`);
+
+--
+-- Indices de la tabla `destino`
+--
+ALTER TABLE `destino`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_destino_idviaje` (`idViaje`),
+  ADD KEY `fk_destino_agregadoPor` (`agregadoPor`);
+
+--
+-- Indices de la tabla `destinovotado`
+--
+ALTER TABLE `destinovotado`
+  ADD PRIMARY KEY (`idUsuario`,`idViaje`,`idDestino`),
+  ADD KEY `fk_destinovotado_idDestino` (`idDestino`);
+
+--
+-- Indices de la tabla `plan`
+--
+ALTER TABLE `plan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_plan_idDestino` (`idDestino`),
+  ADD KEY `fk_plan_agregadorPor` (`agregadoPor`);
+
+--
+-- Indices de la tabla `planvotado`
+--
+ALTER TABLE `planvotado`
+  ADD PRIMARY KEY (`idUsuario`,`idViaje`,`idPlan`),
+  ADD KEY `fk_planvotado_idPlan` (`idPlan`);
+
+--
+-- Indices de la tabla `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tag_idDestino` (`idDestino`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`nickname`),
+  ADD UNIQUE KEY `index_email` (`email`);
+
+--
+-- Indices de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_viaje_idusuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `viajero`
+--
+ALTER TABLE `viajero`
+  ADD PRIMARY KEY (`idUsuario`,`idViaje`),
+  ADD KEY `fk_viajero_idviaje` (`idViaje`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `destino`
+--
+ALTER TABLE `destino`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `plan`
+--
+ALTER TABLE `plan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `colaborador`
+--
+ALTER TABLE `colaborador`
+  ADD CONSTRAINT `fk_colaborador_idusuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_colaborador_idviaje` FOREIGN KEY (`idViaje`) REFERENCES `viaje` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `destino`
+--
+ALTER TABLE `destino`
+  ADD CONSTRAINT `fk_destino_agregadoPor` FOREIGN KEY (`agregadoPor`) REFERENCES `usuario` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_destino_idviaje` FOREIGN KEY (`idViaje`) REFERENCES `viaje` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `destinovotado`
+--
+ALTER TABLE `destinovotado`
+  ADD CONSTRAINT `fk_destinovotado_idDestino` FOREIGN KEY (`idDestino`) REFERENCES `destino` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_destinovotado_idUsiario_idViaje` FOREIGN KEY (`idUsuario`,`idViaje`) REFERENCES `viajero` (`idUsuario`, `idViaje`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `plan`
+--
+ALTER TABLE `plan`
+  ADD CONSTRAINT `fk_plan_agregadorPor` FOREIGN KEY (`agregadoPor`) REFERENCES `usuario` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_plan_idDestino` FOREIGN KEY (`idDestino`) REFERENCES `destino` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `planvotado`
+--
+ALTER TABLE `planvotado`
+  ADD CONSTRAINT `fk_planvotado_idPlan` FOREIGN KEY (`idPlan`) REFERENCES `plan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_planvotado_idUsiario_idViaje` FOREIGN KEY (`idUsuario`,`idViaje`) REFERENCES `viajero` (`idUsuario`, `idViaje`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tag`
+--
+ALTER TABLE `tag`
+  ADD CONSTRAINT `fk_tag_idDestino` FOREIGN KEY (`idDestino`) REFERENCES `destino` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD CONSTRAINT `fk_viaje_idusuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `viajero`
+--
+ALTER TABLE `viajero`
+  ADD CONSTRAINT `fk_viajero_idusuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_viajero_idviaje` FOREIGN KEY (`idViaje`) REFERENCES `viaje` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ documentacion/datos_de_prueba.sql
@@ -0,0 +1,113 @@
+
+INSERT INTO `usuario` (`nickname`, `email`, `contrasenia`, `nombre`, `apellido`, `telefono`, `biografia`, `imagen`, `verificado`) VALUES 
+('antonio57', 'antonio@email.com', '1234', 'Antonio', 'Garcia', '098432872', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem reprehenderit magnam, tempora aspernatur eum quos consectetur nesciunt, iusto commodi repellat unde. Amet pariatur officia assumenda!', null, true),
+('marianela42', 'marianela@email.com', '1234', 'Maria', 'Martinez', '098147584', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo fugiat, nesciunt accusantium incidunt excepturi rem. Illum est provident nobis totam!', null, true),
+('jose57', 'jose@email.com', '1234', 'Jose', 'Lopez', '095128757', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, numquam ipsum nulla illo reprehenderit veniam vel voluptatibus facere nisi quibusdam accusantium dolores.', null, true),
+('maria98', 'maria@email.com', '1234', 'Maria Carmen', 'Sanchez', '095784589', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et eaque illo sapiente culpa voluptatem maiores delectus inventore velit.', null, true),
+('francisco91', 'francisco@email.com', '1234', 'Francisco', 'Gonzalez', '099887543', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam quibusdam aliquam maxime. ', null, true),
+('josefa84', 'josefa@email.com', '1234', 'Josefa', 'Gomez', '092784256', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta non eaque iure perferendis, autem earum, reprehenderit laboriosam perspiciatis facere dolorem ipsam suscipit laudantium! ', null, true);
+
+INSERT INTO `viaje` (`id`, `nombre`, `descripcion`, `publico`, `realizado`, `idUsuario`) VALUES 
+(1, 'Vinos de América', 'Oportundad para recrear la cultura vitivinícola.', true, true, 'antonio57'),
+(2, 'Corredor termal Río Uruguay.', 'Aguas termales, un encanto para disfrutar.', true, false, 'jose57'),
+(3, 'Uruguay Natural.', 'De la costa a las sierras.', false, false, 'antonio57'),
+(4, 'El gran Buenos Aires.', 'Paseos y entretenimiento.', false, false, 'francisco91');
+
+INSERT INTO `destino` (`id`, `pais`, `ciudad`, `idViaje`, `agregadoPor`, `fechaAgregado`) VALUES 
+(1, 'Argentina', 'Mendoza', 1, 'antonio57', '2020-04-12'),
+(2, 'Chile', 'Santiago', 1, 'francisco91', '2020-04-13'),
+(3, 'Uruguay', 'Salto', 2, 'jose57', '2020-04-14'),
+(4, 'Argentina', 'Colón', 2, 'maria98', '2020-04-15'),
+(5, 'Uruguay', 'Piriápolis', 3, 'josefa84', '2020-04-16'),
+(6, 'Uruguay', 'Minas', 3, 'maria98', '2020-04-17'),
+(7, 'Argentina', 'Buenos Aires', 4, 'francisco91', '2020-04-18'),
+(8, 'Argentina', 'Tigre.', 4, 'maria98', '2020-04-19');
+
+INSERT INTO `tag` (`id`, `texto`, `idDestino`) VALUES 
+(1, 'Brindis Rass', 1),
+(2, 'Restoran Rass', 1),
+(3, 'Bodega Hum', 2),
+(4, 'Vendimia', 2),
+(5, 'Termas', 3),
+(6, 'Juegos acuáticos', 3),
+(7, 'Termas', 4),
+(8, 'Compras', 4),
+(9, 'Aventura', 5),
+(10, 'Playa.', 5),
+(11, 'Sierra.', 6),
+(12, 'Festival.', 6),
+(13, 'Teatro.', 7),
+(14, 'Cena.', 7),
+(15, 'Hospedaje.', 8),
+(16, 'Paseos.', 8);
+
+INSERT INTO `plan` (`id`, `nombre`, `descripcion`, `latitud`, `longitud`, `link`, `idDestino`, `agregadoPor`, `fechaAgregado`) VALUES 
+(1, 'Degustación de vinos.', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ', -64.45312858, 50.66033345, 'http://www.ejemplo.com/plan-1', 1, 'antonio57', '2020-04-18'),
+(2, 'Almuerzo con copa de vino.', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ', 5.85937142, 17.21588078, 'http://www.ejemplo.com/plan-2', 1, 'maria98', '2020-04-19'),
+(3, 'Visita a bodega Hum. ', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ', 100.07812142, 17.21588078, 'null', 2, 'francisco91', '2020-04-20'),
+(4, 'Recreación de la vendimia.', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ', 138.04687142, 17.21588078, 'http://www.ejemplo.com/plan-4', 2, 'maria98', '2020-04-21'),
+(5, 'Termas de Daymán.', 'Dos días de piscinas termales. ', 138.04687142, 44.00512491, 'null', 3, 'jose57', '2020-04-22'),
+(6, 'Aquagame Daymán.', 'Dos días con entrada incluída al parque de juegos.', 5.85937142, 50.66033345, 'http://www.ejemplo.com/plan-6', 3, 'josefa84', '2020-04-23'),
+(7, 'Termas Colón. ', 'Dos días de piscinas termales.', 100.07812142, 50.66033345, 'http://www.ejemplo.com/plan-7', 4, 'maria98', '2020-04-24'),
+(8, 'Rambla de artesanos.', 'Tour de compras.', 138.04687142, 50.66033345, 'http://www.ejemplo.com/plan-8', 4, 'maria98', '2020-04-25'),
+(9, 'Cerro Pan de Azúcar.', 'Escalada al cerro a cargo de especialistas.', -64.45312858, 17.21588078, 'null', 5, 'josefa84', '2020-04-26'),
+(10, 'Playa.', 'Arena, sol y agua.', 100.07812142, 44.00512491, 'null', 5, 'antonio57', '2020-04-27'),
+(11, 'Cerro Arequita.', 'Reconocimiento de flora y fauna.', -64.45312858, -25.2312458, 'http://www.ejemplo.com/plan-11', 6, 'maria98', '2020-04-28'),
+(12, 'Cerro Artigas.', 'Noche de los fogones.', 100.07812142, -25.2312458, 'http://www.ejemplo.com/plan-12', 6, 'josefa84', '2020-04-29'),
+(13, 'Teatro Plaza.', 'Les Luthiers.', 5.85937142, 44.00512491, 'http://www.ejemplo.com/plan-13', 7, 'francisco91', '2020-04-30'),
+(14, 'Cena con amigos.', 'Restoran Los luteros.', 5.85937142, -25.2312458, 'http://www.ejemplo.com/plan-14', 7, 'maria98', '2020-05-01'),
+(15, 'Ubicación en hospedaje.', 'Hostel del delta.', -64.45312858, 44.00512491, 'http://www.ejemplo.com/plan-15', 8, 'maria98', '2020-05-02'),
+(16, 'Navegación por canales.', 'Navegación por los canales y paseo de compras.', 138.04687142, -25.2312458, 'null', 8, 'francisco91', '2020-05-03');
+
+INSERT INTO `viajero` (`idUsuario`, `idViaje`, `valoracion`, `texto`) VALUES 
+('marianela42', 1, 5, 'La verdad que un viaje muy recomendable'),
+('jose57', 1, 5, 'Se pasó muy bien, ojalá se repita'),
+('josefa84', 1, 4, 'Muy bueno el viaje, pero tendría que haber durado más!'),
+('maria98', 2, null, null),
+('francisco91', 2, null, null),
+('josefa84', 2, null, null),
+('antonio57', 2, null, null),
+('maria98', 3, null, null);
+
+INSERT INTO `colaborador` (`idUsuario`, `idViaje`) VALUES 
+('francisco91', 1),
+('maria98', 1),
+('josefa84', 3),
+('maria98', 4);
+
+
+/* ****************************************************************************** */
+INSERT INTO `viajero` (`idUsuario`, `idViaje`)
+    SELECT u.nickname as idUsuario, v.id as idViaje
+    FROM usuario u JOIN viaje v ON v.idUsuario = u.nickname
+;
+
+INSERT INTO `destinovotado` (`idUsuario`, `idViaje`, `idDestino`) 
+    SELECT sc.nickname, sc.idViaje, d.id as idDestino
+    FROM (
+        SELECT vj.idUsuario as nickname, vj.idViaje as idviaje
+        FROM viajero vj
+        UNION
+        SELECT u.nickname as nickname, v.id as idviaje
+        FROM usuario u JOIN viaje v ON v.idUsuario = u.nickname
+    ) sc JOIN destino d ON d.idViaje = sc.idViaje
+    WHERE (sc.idViaje = 1)
+    OR (sc.idViaje <> 1 AND sc.nickname LIKE '%e%')
+;
+
+INSERT INTO `planvotado` (`idUsuario`, `idViaje`, `idPlan`) 
+	SELECT sc.nickname, sc.idViaje, p.id as idPlan
+	FROM (
+		SELECT vj.idUsuario as nickname, vj.idViaje as idviaje
+		FROM viajero vj
+		UNION
+		SELECT u.nickname as nickname, v.id as idviaje
+		FROM usuario u JOIN viaje v ON v.idUsuario = u.nickname
+	) sc JOIN destino d ON d.idViaje = sc.idViaje
+	JOIN plan p ON p.idDestino = d.id
+	WHERE (sc.idViaje = 1)
+	OR (sc.idViaje <> 1 AND sc.nickname LIKE '%e%')
+;
+
+
+

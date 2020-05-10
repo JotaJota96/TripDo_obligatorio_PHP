ae7b147f32c83339a048a98c6d27b590f6f7c555
Julio Arrieta <jaservice@vera.com.uy>
2020-05-09 22:40:45 -0300

    inicio de la pagina index

--- tripdo/application/views/welcome_message.php
+++ tripdo/application/views/welcome_message.php
@@ -1,89 +1,286 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
+	<title>Destino</title>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Destino project">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/bootstrap4/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/main_styles.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/inex_style.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/index_reponsive.css">
 </head>
+
 <body>
+	<div class="super_container">
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+		<!-- Header -->
+		<header class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div
+							class=" header_container fixed-top d-flex flex-row align-items-center justify-content-start">
+							<!-- Logo -->
+							<div class="logo_container ">
+								<div class="logo">
+									<div>TripDo</div>
+									<div>travel agency</div>
+									<div class="logo_image"><img src="<?= base_url()?>/public/images/logo.png" alt=""></div>
+								</div>
+							</div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+							<!-- Main Navigation -->
+							<nav class="main_nav ml-auto ">
+								<ul class="main_nav_list ">
+									<li class="main_nav_item active"><a href="index.html">Inicio</a></li>
+									<li class="main_nav_item"><a href="#">About us</a></li>
+									<li class="main_nav_item"><a href="busqueda.html">Buscar</a></li>
+									<li class="main_nav_item"><a href="viaje.html">Viaje</a></li>
+									<li class="main_nav_item"><a href="#">Contact</a></li>
+								</ul>
+							</nav>
+							<!-- Hamburger -->
+							<div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<!-- Menu -->
+		<div class="menu_container menu_mm">
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+			<!-- Menu Close Button -->
+			<div class="menu_close_container">
+				<div class="menu_close"></div>
+			</div>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+			<!-- Menu Items -->
+			<div class="menu_inner menu_mm">
+				<div class="menu menu_mm">
+					<div class="menu_search_form_container">
+						<form action="#" id="menu_search_form">
+							<input type="search" class="menu_search_input menu_mm">
+							<button id="menu_search_submit" class="menu_search_submit" type="submit"><img
+									src="<?= base_url()?>/public/images/search_2.png" alt=""></button>
+						</form>
+					</div>
+					<ul class="menu_list menu_mm">
+						<li class="menu_item menu_mm"><a href="#">Home</a></li>
+						<li class="menu_item menu_mm"><a href="about.html">About us</a></li>
+						<li class="menu_item menu_mm"><a href="offers.html">Offers</a></li>
+						<li class="menu_item menu_mm"><a href="news.html">News</a></li>
+						<li class="menu_item menu_mm"><a href="contact.html">Contact</a></li>
+					</ul>
+
+					<!-- Menu Social -->
+
+					<div class="menu_social_container menu_mm">
+						<ul class="menu_social menu_mm">
+							<li class="menu_social_item menu_mm"><a href="#"><i class="fa fa-pinterest"
+										aria-hidden="true"></i></a></li>
+							<li class="menu_social_item menu_mm"><a href="#"><i class="fa fa-linkedin"
+										aria-hidden="true"></i></a></li>
+							<li class="menu_social_item menu_mm"><a href="#"><i class="fa fa-instagram"
+										aria-hidden="true"></i></a></li>
+							<li class="menu_social_item menu_mm"><a href="#"><i class="fa fa-facebook"
+										aria-hidden="true"></i></a></li>
+							<li class="menu_social_item menu_mm"><a href="#"><i class="fa fa-twitter"
+										aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+
+					<div class="menu_copyright menu_mm">Colorlib All rights reserved</div>
+				</div>
+
+			</div>
+
+		</div>
+
+		<!-- Home -->
+		<div class="home">
+			<div class="home_background" style="background-image:url(<?= base_url()?>/public/images/home.jpg)"></div>
+			<div class="home_content">
+				<div class="home_content_inner">
+					<div class="home_text_large">TRIPDO</div>
+					<div class="home_text_small">Discover new worlds</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Popular -->
+		<div class="popular">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="section_title text-center">
+							<h2>Popular destinations in 2018</h2>
+							<div>take a look at these offers</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div
+							class="popular_content d-flex flex-md-row flex-column flex-wrap align-items-md-center align-items-start justify-content-md-between justify-content-start">
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_1.jpg" alt="image by Egzon Bytyqi">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Turkey</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_2.jpg" alt="https://unsplash.com/@michael75">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Hawai</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_3.jpg" alt="https://unsplash.com/@sapegin">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Ireland</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_4.jpg" alt="https://unsplash.com/@kensuarez">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Thailand</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_5.jpg" alt="https://unsplash.com/@noahbasle">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Croatia</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_6.jpg" alt="https://unsplash.com/@seb">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Bali</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_7.jpg" alt="https://unsplash.com/@nevenkrcmarek">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">France</div>
+									</div>
+								</a>
+							</div>
+
+							<!-- Popular Item -->
+							<div class="popular_item">
+								<a href="offers.html">
+									<img src="<?= base_url()?>/public/images/popular_8.jpg" alt="https://unsplash.com/@bergeryap87">
+									<div class="popular_item_content">
+										<div class="popular_item_price">From $890</div>
+										<div class="popular_item_title">Vietnam</div>
+									</div>
+								</a>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Footer -->
+		<footer class="footer">
+			<div class="container">
+				<div class="row">
+					<!-- Footer Column -->
+					<div class="col-lg-8 footer_col">
+						<div class="footer_about">
+							<!-- Logo -->
+							<div class="logo_container">
+								<div class="logo">
+									<div>destino</div>
+									<div>travel agency</div>
+									<div class="logo_image"><img src="<?= base_url()?>/public/images/logo.png" alt=""></div>
+								</div>
+							</div>
+							<div class="footer_about_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Integer pulvinar sed mauris eget tincidunt. Sed lectus nulla, tempor vel eleifend quis,
+								tempus rut rum metus. Pellentesque ultricies enim eu quam fermentum hendrerit.</div>
+							<div class="copyright">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								Copyright &copy;
+								<script>document.write(new Date().getFullYear());</script> All rights reserved | This
+								template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a
+									href="https://colorlib.com" target="_blank">Colorlib</a>
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</div>
+						</div>
+					</div>
+					<!-- Footer Column -->
+					<div class="col-lg-4 footer_col">
+						<div class="tags footer_tags">
+							<div class="footer_title">Tags</div>
+							<ul class="tags_content d-flex flex-row flex-wrap align-items-start justify-content-start">
+								<li class="tag"><a href="#">travel</a></li>
+								<li class="tag"><a href="#">summer</a></li>
+								<li class="tag"><a href="#">cruise</a></li>
+								<li class="tag"><a href="#">beach</a></li>
+								<li class="tag"><a href="#">offer</a></li>
+								<li class="tag"><a href="#">vacation</a></li>
+								<li class="tag"><a href="#">trip</a></li>
+								<li class="tag"><a href="#">city break</a></li>
+								<li class="tag"><a href="#">adventure</a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</footer>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
+	<script src="<?= base_url()?>/public/js/jquery-3.2.1.min.js"></script>
+	<script src="<?= base_url()?>/public/styles/bootstrap4/popper.js"></script>
+	<script src="<?= base_url()?>/public/styles/bootstrap4/bootstrap.min.js"></script>
+	<script src="<?= base_url()?>/public/js/custom.js"></script>
 </body>
+
 </html>

384f9efaf5061d44915872f9d46102ebe878baa1
PricKG <lucas,garrido@utec.edu.uy>
2020-05-10 00:19:29 -0300

    Integracion de los html con el proyecto Codeignaiter

--- tripdo/application/views/busqueda.php
+++ tripdo/application/views/busqueda.php
@@ -2,11 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<title>Destino</title>
+	<title>Offers</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="description" content="Destino project">
@@ -14,14 +15,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<link href="<?= base_url()?>/public/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	<link href="<?= base_url()?>/public/plugins/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/bootstrap4/bootstrap.min.css">
+	
 	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/plugins/OwlCarousel2-2.2.1/animate.css">
+	
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/bootstrap4/bootstrap.min.css">
+	
 	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/main_styles.css">
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/inex_style.css">
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/index_reponsive.css">
-
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/busqueda_style.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/public/styles/busqueda_reponsive.css">
+	
 </head>
 
 <body>
@@ -111,115 +115,136 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		</div>
 
-		<!-- Home -->
-		<div class="home">
-			<div class="home_background" style="background-image:url(<?= base_url()?>/public/images/home.jpg)"></div>
-			<div class="home_content">
-				<div class="home_content_inner">
-					<div class="home_text_large">TRIPDO</div>
-					<div class="home_text_small">Discover new worlds</div>
-				</div>
-			</div>
-		</div>
-		
-		<!-- Popular -->
-		<div class="popular">
+		<!-- Offers -->
+		<div class="offers">
 			<div class="container">
 				<div class="row">
 					<div class="col">
 						<div class="section_title text-center">
-							<h2>Destinos populares</h2>
+							<h2>Top destinations in Europe</h2>
 						</div>
 					</div>
 				</div>
+
+				<div class="menu_search_form_container">
+					<form action="#" id="menu_search_form">
+						<input type="search" class="menu_search_input menu_mm">
+						<button id="menu_search_submit" class="menu_search_submit" type="submit"><img
+								src="<?= base_url()?>/public/images/search_2.png" alt=""></button>
+					</form>
+				</div>
+
 				<div class="row">
 					<div class="col">
-						<div
-							class="popular_content d-flex flex-md-row flex-column flex-wrap align-items-md-center align-items-start justify-content-md-between justify-content-start">
+						<div class="items item_grid clearfix">
 
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_1.jpg" alt="image by Egzon Bytyqi">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Turkey</div>
+							<!-- Item -->
+							<div class="item clearfix rating_5">
+								<div class="item_image"><img src="<?= base_url()?>/public/images/top_1.jpg" alt=""></div>
+								<div class="item_content">
+									<div class="item_title">Paris, France</div>
+									<div class="rating rating_5" data-rating="5">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
 									</div>
-								</a>
+									<div class="item_text">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis. Quisque vel
+										est id justo faucibus dapibus id a nibh. Aenean suscipit consequat lacus, sit
+										amet mollis nulla. Morbi sagittis orci id lacus convallis tempus eget sit amet
+										metus.
+									</div>
+								</div>
 							</div>
 
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_2.jpg" alt="https://unsplash.com/@michael75">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Hawai</div>
+							<!-- Item -->
+							<div class="item clearfix rating_3">
+								<div class="item_image"><img src="<?= base_url()?>/public/images/top_2.jpg" alt=""></div>
+								<div class="item_content">
+									<div class="item_title">Cinque Terre</div>
+									
+									<div class="rating rating_3" data-rating="3">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
 									</div>
-								</a>
+									<div class="item_text">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis. Quisque vel
+										est id justo faucibus dapibus id a nibh. Aenean suscipit consequat lacus, sit
+										amet mollis nulla. Morbi sagittis orci id lacus convallis tempus eget sit amet
+										metus.
+									</div>
+								</div>
 							</div>
 
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_3.jpg" alt="https://unsplash.com/@sapegin">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Ireland</div>
+							<!-- Item -->
+							<div class="item clearfix rating_4">
+								<div class="item_image"><img src="<?= base_url()?>/public/images/top_3.jpg" alt=""></div>
+								<div class="item_content">
+									<div class="item_title">Italian Riviera</div>
+									<div class="rating rating_4" data-rating="4">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
 									</div>
-								</a>
+									<div class="item_text">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis. Quisque vel
+										est id justo faucibus dapibus id a nibh. Aenean suscipit consequat lacus, sit
+										amet mollis nulla. Morbi sagittis orci id lacus convallis tempus eget sit amet
+										metus.
+									</div>
+								</div>
 							</div>
 
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_4.jpg" alt="https://unsplash.com/@kensuarez">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Thailand</div>
-									</div>
-								</a>
-							</div>
+							<!-- Item -->
+							<div class="item clearfix rating_5">
+								<div class="item_image"><img src="<?= base_url()?>/public/images/top_4.jpg" alt=""></div>
+								<div class="item_content">
+									<div class="item_title">Santorini, Greece</div>
 
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_5.jpg" alt="https://unsplash.com/@noahbasle">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Croatia</div>
+									<div class="rating rating_5" data-rating="5">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
 									</div>
-								</a>
-							</div>
-
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_6.jpg" alt="https://unsplash.com/@seb">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Bali</div>
+									<div class="item_text">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis. Quisque vel
+										est id justo faucibus dapibus id a nibh. Aenean suscipit consequat lacus, sit
+										amet mollis nulla. Morbi sagittis orci id lacus convallis tempus eget sit amet
+										metus.
 									</div>
-								</a>
-							</div>
-
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_7.jpg" alt="https://unsplash.com/@nevenkrcmarek">
-									<div class="popular_item_content">
-										<div class="popular_item_title">France</div>
-									</div>
-								</a>
-							</div>
-
-							<!-- Popular Item -->
-							<div class="popular_item">
-								<a href="offers.html">
-									<img src="<?= base_url()?>/public/images/popular_8.jpg" alt="https://unsplash.com/@bergeryap87">
-									<div class="popular_item_content">
-										<div class="popular_item_title">Vietnam</div>
-									</div>
-								</a>
+								</div>
 							</div>
 
 						</div>
 					</div>
 				</div>
+				<!--
+				<div class="row">
+					<div class="col">
+						<div class="pages">
+							<ul class="pages_list">
+								<li class="page active"><a href="#">01.</a></li>
+								<li class="page"><a href="#">02.</a></li>
+								<li class="page"><a href="#">03.</a></li>
+								<li class="page"><a href="#">04.</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				-->
 			</div>
 		</div>
 
@@ -268,11 +293,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							</ul>
 						</div>
 					</div>
+
 				</div>
 			</div>
 		</footer>
 	</div>
-
 
 	<script src="<?= base_url()?>/public/js/jquery-3.2.1.min.js"></script>
 	<script src="<?= base_url()?>/public/styles/bootstrap4/popper.js"></script>
@@ -280,7 +305,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="<?= base_url()?>/public/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
 	<script src="<?= base_url()?>/public/plugins/easing/easing.js"></script>
 	<script src="<?= base_url()?>/public/plugins/parallax-js-master/parallax.min.js"></script>
-	<script src="<?= base_url()?>/publicp/lugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+	<script src="<?= base_url()?>/public/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
 	<script src="<?= base_url()?>/public/js/custom.js"></script>
 </body>
 

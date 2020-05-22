689a580927f8b177a9ceda525552a0eed7e8f5d9
JotaJota96 <jjap96@gmail.com>
2020-05-22 02:52:28 -0300

    Retoque en formulario de Registrarse y Crear viaje

--- tripdo/application/views/crear_viaje.php
+++ tripdo/application/views/crear_viaje.php
@@ -10,11 +10,11 @@
                 <div class="col-lg-8">
                     <div class="form_registro">
                         <form action="<?= base_url('crearViaje/validate')?>" method="POST" >
-                            <div class="form-group row col-md-9">
+                            <div class="form-group row col-md-9 mx-auto">
                                 <h2 class="col-md-9">Crear Nuevo Viaje</h2>
                             </div>
 
-                            <div class="form-group row col-md-9">
+                            <div class="form-group row col-md-9 mx-auto">
                                 <label class="col-md-3" for="nombreViaje">Nombre del Viaje</label>
                                 <div class="col-md-9">
                                     <input type="text" class="form-control" id="nombreViaje" name="nombreViaje">
@@ -22,7 +22,7 @@
                                 </div>																
                             </div>
                             
-                            <div class="form-group row col-md-9">
+                            <div class="form-group row col-md-9 mx-auto">
                                 <label class="col-md-3" for="descripcion">Descripción</label>
                                 <div class="col-md-9">
                                 <textarea class="form-control" id="descripcion" name="descripcion" rows="5"></textarea>
@@ -30,7 +30,7 @@
                                 </div>								
                             </div>  
                             
-                            <div class="form-group row col-md-9">
+                            <div class="form-group row col-md-9 mx-auto">
                                 <div class="form-check col-md-4"></div>
                                 <div class="form-check col-md-4">
                                     <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
@@ -48,7 +48,7 @@
                             </div>                                      
                             <div class="form-check col-md-3"></div>               
                             <div class="form-goup row">
-                                <div class="col-md-9">
+                                <div class="col-md-9 mx-auto">
                                     <button type="submit" class="_button btn-block">Crear Vieja</button>
                                 </div>
                             </div>
--- tripdo/application/views/registro.php
+++ tripdo/application/views/registro.php
@@ -14,77 +14,77 @@
 							<div class="form-group">
 								<h2 class="col-md-9">Registro</h2>
 							</div>							
-							<div class="form-group row col-md-9">
+							<div class="form-group row col-md-9 mx-auto">
 								<label class="col-md-3" for="nickname">Ninckname</label>
-								<div class="col-md-9">
+								<div class="col-md-9 mx-auto">
 									<input type="text" class="form-control" id="ninckname" name="nickname" value="<?= $defNick?>">
 									<span><?= form_error('nickname'); ?></span>	
 								</div>																
 							</div>
 							
-							<div class="form-group row col-md-9">
+							<div class="form-group row col-md-9 mx-auto">
 							<label class="col-md-3" for="contrasenia">Contraseña</label>
-								<div class="col-md-9">
+								<div class="col-md-9 mx-auto">
 									<input type="password" class="form-control" id="contrasenia" name="contrasenia">
 									<span><?= form_error('contrasenia'); ?></span>	
 								</div>								
 							</div>
 							
-							<div class="form-group row col-md-9">
+							<div class="form-group row col-md-9 mx-auto">
 								<label class="col-md-3" for="contrasenia2">Confirmar Contraseña</label>
-								<div class="col-md-9">
+								<div class="col-md-9 mx-auto">
 									<input type="password" class="form-control" id="contrasenia2" name="contrasenia2">
 									<span><?= form_error('contrasenia2'); ?></span>	
 								</div>								
 							</div>
 
-							<div class="form-group row col-md-9">
+							<div class="form-group row col-md-9 mx-auto">
 								<label class="col-md-3" for="email">Correo</label>
-								<div class="col-md-9">
+								<div class="col-md-9 mx-auto">
 									<input type="email" class="form-control" id="email" name="email" value="<?= $defEmail?>">	
 									<span><?= form_error('email'); ?></span>	
 								</div>																
 							</div>
 
-							<div class="form-group row col-md-9">
+							<div class="form-group row col-md-9 mx-auto">
 								<label class="col-md-3" for="nombre">Nombre</label>
-								<div class="col-md-9">
+								<div class="col-md-9 mx-auto">
 									<input type="text" class="form-control" id="nombre" name="nombre" value="<?= $defNombre?>">
 									<span><?= form_error('nombre'); ?></span>	
 								</div>
 							</div>
 							
-								<div class="form-group row col-md-9">
+								<div class="form-group row col-md-9 mx-auto">
 									<label class="col-md-3" for="apellido">Apellido</label>
-									<div class="col-md-9">
+									<div class="col-md-9 mx-auto">
 										<input type="text" class="form-control" id="apellido" name="apellido" value="<?= $defApellido?>">
 										<span><?= form_error('apellido'); ?></span>	
 									</div>
 								</div>
 
-								<div class="form-group col-md-9 row">
+								<div class="form-group col-md-9 mx-auto row">
 									<label class="col-md-3" for="telefono">Teléfono *</label>
-									<div class="col-md-9">
+									<div class="col-md-9 mx-auto">
 										<input type="phone" class="form-control" id="telefono" name="telefono" value="<?= $defTel?>">
 									</div>
 								</div>
 
-								<div class="form-group row col-md-9">
+								<div class="form-group row col-md-9 mx-auto">
 									<label class="col-md-3" for="biografia">Biografía *</label>
-									<div class="col-md-9">
+									<div class="col-md-9 mx-auto">
     									<textarea class="form-control" id="biografia" rows="4" name="biografia" ><?= $defBiog?></textarea>
 									</div>
 								</div>	
 
-								<div class="form-group row col-md-9">
+								<div class="form-group row col-md-9 mx-auto">
 									<label class="col-md-3" for="foto">Subir Foto *</label>
-									<div class="col-md-9">
+									<div class="col-md-9 ">
 										<input type="file" class="form-control-file" id="foto">
 									</div>
 								</div>	
 								
 								<div class="form-goup row">
-									<div class="col-md-9">
+									<div class="col-md-9 mx-auto">
 										<button type="submit" class="_button btn-block" name="btnregistrar">Registrarse</button>
 										
 										<?php if(isset($exception)){ ?>
@@ -94,7 +94,7 @@
 									</div>
 								</div>
 
-								<div class="form-group row col-md-9 mt-2">
+								<div class="form-group row col-md-9 mx-auto mt-2">
 									<span class="text-muted">Los campos con * son opcionales.</span>
 								</div>
 							</form>

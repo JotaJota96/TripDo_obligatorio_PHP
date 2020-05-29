57ba66ee2e63cece28ba134ad570e8c8c8eb53d5
alfajor <alfa@gmail.com>
2020-05-29 20:46:52 -0300

    hablilitado setear imagen a usuario

--- tripdo/application/controllers/Registro.php
+++ tripdo/application/controllers/Registro.php
@@ -87,7 +87,41 @@ class Registro extends CI_Controller {
 		$dtusuario->apellido = $this->input->post('apellido');
 		$dtusuario->telefono = $this->input->post('telefono');
 		$dtusuario->biografia = $this->input->post('biografia');
-		//$dtusuario->imagen = $this->input->post('imagen');
+
+		//-------------------aca empieza el tema imagen--------------------------
+		
+        // Check if file was uploaded without errors
+		if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
+			$allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+			$filename = $_FILES["photo"]["name"];
+			$filetype = $_FILES["photo"]["type"];
+			$filesize = $_FILES["photo"]["size"];
+		
+			// Verify file extension
+			$ext = pathinfo($filename, PATHINFO_EXTENSION);
+			if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+		
+			// Verify file size - 5MB maximum
+			//$maxsize = 5 * 1024 * 1024;
+			$maxsize = 10 * 5000 * 5000; //yo lo cambie para que aguante mas
+			if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+		
+			// Verify MYME type of the file
+			if(in_array($filetype, $allowed)){
+
+				move_uploaded_file($_FILES["photo"]["tmp_name"], "public/perfiles/" . $nick . "." . $ext);
+				echo "Your file was uploaded successfully.";
+				$dtusuario->imagen = $this->input->post($nick . "." . $ext);
+				
+			} else{
+				echo "Error: There was a problem uploading your file. Please try again."; 
+			}
+		} else{
+			echo "Error: " . $_FILES["photo"]["error"];
+		}
+
+        
+		//-------------------termina imagen--------------------------
 
 		try {
 			//generar el codigo 

00452e58b2972c7bf4193b6252d7f86b47fd34d4
Julio Arrieta <jaservice@vera.com.uy>
2020-06-06 20:01:18 -0300

    Se agregan los juego de caracteres para acentos en la clase de validación alpha_numeric_spaces.

--- tripdo/system/libraries/Form_validation.php
+++ tripdo/system/libraries/Form_validation.php
@@ -1327,7 +1327,7 @@ class CI_Form_validation {
 	 */
 	public function alpha_numeric_spaces($str)
 	{
-		return (bool) preg_match('/^[A-Z0-9 ]+$/i', $str);
+		return (bool) preg_match('/^[A-Z0-9 áéíóúüÁÉÍÓÚÜ ]+$/i', $str);
 	}
 
 	// --------------------------------------------------------------------

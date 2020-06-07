1f62cb24bcc3963e003163630114901b1aa9d147
JotaJota96 <jjap96@gmail.com>
2020-06-06 21:04:05 -0300

    Correccion: Se mostraban votones para votar en viajes ya realizados

--- tripdo/application/views/viaje.php
+++ tripdo/application/views/viaje.php
@@ -103,7 +103,7 @@
 									if (count($destinos) > 0){
 										foreach($destinos as $d){
 											// modificar la linea para decidir si el destino ya fue votado por el usuario o no
-											$mostrarVotarDestino = isset($idDestinosVotados) && !(in_array($d->id,$idDestinosVotados));
+											$mostrarVotarDestino = (!$viaje->realizado) && isset($idDestinosVotados) && !(in_array($d->id,$idDestinosVotados));
 											$col_destino = "col-10";
 											if ( ! $mostrarVotarDestino) $col_destino = "col-12";
 								?>
@@ -124,7 +124,7 @@
 	                                                <?php 
 														foreach ($planes[$d->id] as $p){
 															// Cambiar la linea siguiente y evaluar si el usuario ya voto el plan o no
-															$mostrarVotarPlan = isset($idPlanesVotados) &&  !($mostrarVotarDestino) && !(in_array($p->id,$idPlanesVotados));
+															$mostrarVotarPlan = (!$viaje->realizado) && isset($idPlanesVotados) &&  !($mostrarVotarDestino) && !(in_array($p->id,$idPlanesVotados));
 															$col_plan = "col-10";
 															if ( ! $mostrarVotarPlan) $col_plan = "col-12";
 													?>
